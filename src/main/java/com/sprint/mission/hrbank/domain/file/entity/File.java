79f11061a1c3359fcfbcacffd02d2fc0566bea5c
raonPsm <s102075@naver.com>
2026-03-14 18:29:48 +0900

    fix: File 엔티티 위치 수정
    File 엔티티 내용을 FileController에 잘못 작성하여 해당 문제를
    수정하였다.
    
    Resolves: #35

--- src/main/java/com/sprint/mission/hrbank/domain/file/entity/File.java
+++ src/main/java/com/sprint/mission/hrbank/domain/file/entity/File.java
@@ -1,5 +1,34 @@
 package com.sprint.mission.hrbank.domain.file.entity;
 
-public class File {
+import com.sprint.mission.hrbank.domain.baseentity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "files")
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class File extends BaseEntity {
+
+  // id
+
+  @Column(nullable = false)
+  private String originalName;
+
+  @Column(nullable = false)
+  private String storedName;
+
+  @Column(nullable = false)
+  private String contentType;
+
+  @Column(nullable = false)
+  private Long size;
+
+  @Column(nullable = false)
+  private String storagePath;
+
+  // createdAt
 }
