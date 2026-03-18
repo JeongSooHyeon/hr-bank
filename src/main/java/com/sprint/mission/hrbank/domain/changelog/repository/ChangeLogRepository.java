8b38616d4a743623c89dd4174e60632f78cbd217
JeongSooHyeon <jsh75721@naver.com>
2026-03-18 17:33:14 +0900

    fix: PostgreSQL null 파라미터 타입 오류 수정
    - c.type을 String으로 캐스팅하는 필요없는 코드 삭제
    
    Resolves: #118

--- src/main/java/com/sprint/mission/hrbank/domain/changelog/repository/ChangeLogRepository.java
+++ src/main/java/com/sprint/mission/hrbank/domain/changelog/repository/ChangeLogRepository.java
@@ -20,7 +20,7 @@ public interface ChangeLogRepository extends JpaRepository<ChangeLog, Long> {
       // 사번 부분 일치
       + "AND (:memo IS NULL OR c.memo LIKE %:memo%) " // 메모 부분 일치
       + "AND (:ipAddress IS NULL OR c.ipAddress LIKE %:ipAddress%) "  // ip 부분 일치
-      + "AND (:type IS NULL OR c.type = CAST(:type AS string)) " // 유형 완전 일치
+      + "AND (:type IS NULL OR c.type = :type) " // 유형 완전 일치
       + "AND (CAST(:atFrom AS timestamp) IS NULL OR c.createdAt >= :atFrom) " // 시간 범위 ~부터
       + "AND (CAST(:atTo AS timestamp) IS NULL OR c.createdAt <= :atTo)")
   // 시간 범위 ~까지
